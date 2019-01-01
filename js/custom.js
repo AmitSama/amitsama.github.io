8314f163885d55bc0f6fc5d1841cf1920c5db12a
amitsama <amit.sam772@gmail.com>
2019-01-01 18:58:37 +0100

    updated callback for animate method, to update url on navigation

--- js/custom.js
+++ js/custom.js
@@ -16,11 +16,15 @@ $(function() {
     if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') || location.hostname == this.hostname) {
 
       var target = $(this.hash);
+      var hash = this.hash;
       target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
       if (target.length) {
         $('html,body').animate({
           scrollTop: target.offset().top
-        }, 1000);
+        }, 1000, function () {
+            window.location.hash = hash;
+        }); //end animate
+
         return false;
       }
     }
